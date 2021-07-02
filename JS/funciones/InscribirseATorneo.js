cedcea381a1dd44f4d78fe4e68b15af3d4261556
ivandipa8 <ivandipa8@gmail.com>
2021-07-02 18:01:04 -0300

    Trabajo terminado v2 :D

--- JS/funciones/InscribirseATorneo.js
+++ JS/funciones/InscribirseATorneo.js
@@ -1,16 +1,18 @@
 ainscripto = [];
-const agregarInscripcion = (torneo, user) =>{
-    var info = ConvertToJSON(GetLocalStorageItem(`${torneo}`));
-    if (!ainscripto.includes(torneo)){
+const agregarInscripcion = (torneo) =>{
+    let id = $(torneo).attr('id')
+    var info = ConvertToJSON(GetLocalStorageItem(`${id}`));
+    console.log(info)
+    if (!ainscripto.includes(id)){
         alert(`Inscripto con suceso al torneo ${info.nombre}, se encuentra en tu lista de Torneos en la parte inferioro de la página a la derecha`)
         $('#inscriptos').append($(`
-            <li id='${torneo}'>
+            <li id='${id}'>
                 <p>${info.nombre}</p>
                 <p>${info.inicio}</p>
                 <p>${info.hora}</p>
             </li>
         `));
-        ainscripto.push(torneo);
+        ainscripto.push(id);
     }else{
         alert('Ya te encuentras inscripto a este torneo.')
     }
